25af537a4581bb748f77dfdc1886123f48c93188
eunicewaeni002 <eunicewaeni002@gmail.com>
2023-03-18 11:08:14 -0700

    Hell, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio .h>
+#include <stdio.h>
 /**
- * main . A  program that prints a line with puts function
- * Return: 0 (sucess)
+ * main -A program that prints a line using prints puts functions
+ * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
